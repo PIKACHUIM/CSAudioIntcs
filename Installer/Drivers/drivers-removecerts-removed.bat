c0e0345354fb797df680e73f5174582aa0be9572
PIKACHUIM <pikachuim@qq.com>
2025-01-25 21:46:20 +0800

    修复声卡驱动

--- Installer/Drivers/drivers-removecerts-removed.bat
+++ Installer/Drivers/drivers-removecerts-removed.bat
@@ -1,3 +1,17 @@
 drivers-installtool-install.exe /F /A /S /U csaudioamdsof.1.0.1-cracked\csaudioamdsof.inf
 drivers-installtool-install.exe /F /A /S /U csaudiointsof.1.0.4-cracked\csaudiointcsof.inf
 drivers-installtool-install.exe /F /A /S /U csaudiosstavs.1.0.4-cracked\csaudiosstavs.inf
+
+drivers-installtool-install.exe /F /A /S /U csaudioothers.1.0.4-origins/acpbus/acpbus.inf
+drivers-installtool-install.exe /F /A /S /U csaudioothers.1.0.4-origins/crosec/croseccodec.inf
+drivers-installtool-install.exe /F /A /S /U csaudioothers.1.0.4-origins/98357a/max98357a.inf
+drivers-installtool-install.exe /F /A /S /U csaudioothers.1.0.4-origins/98390x/max98390.inf
+drivers-installtool-install.exe /F /A /S /U csaudioothers.1.0.4-origins/cs42lx/cs42l42.inf
+drivers-installtool-install.exe /F /A /S /U csaudioothers.1.0.4-origins/da7219/da7219.inf
+drivers-installtool-install.exe /F /A /S /U csaudioothers.1.0.4-origins/na8825/nau8825.inf
+drivers-installtool-install.exe /F /A /S /U csaudioothers.1.0.4-origins/rt568x/rt5682s.inf
+drivers-installtool-install.exe /F /A /S /U csaudioothers.1.0.4-origins/rt1011/rt1011.inf
+drivers-installtool-install.exe /F /A /S /U csaudioothers.1.0.4-origins/rt1015/rt1015.inf
+drivers-installtool-install.exe /F /A /S /U csaudioothers.1.0.4-origins/rt1019/rt1019.inf
+drivers-installtool-install.exe /F /A /S /U csaudioothers.1.0.4-origins/rt5682/rt5682.inf
+drivers-installtool-install.exe /F /A /S /U csaudioothers.1.0.4-origins/xcodec/opengmaxcodec.inf
